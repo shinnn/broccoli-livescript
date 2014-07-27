f868e4bff33e4c9c6fb9ff4dafc6a90a04ca3b74
shinnn <snnskwtnb@gmail.com>
2014-07-27 21:40:52 +0900

    return immediately if file reading failed

--- test/test.js
+++ test/test.js
@@ -8,7 +8,10 @@ var expected = "['ha', 'ha', 'ha'];"; // jshint ignore: line
 describe('broccoli-livescript', function() {
   it('should compile LiveScript to JavaScript', function(done) {
     fs.readFile('test/actual/simple.js', function(err, data) {
-      if (err) done(err);
+      if (err) {
+        done(err);
+        return;
+      }
       assert.strictEqual(data.toString(), expected);
       done();
     });
